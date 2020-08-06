d57e1d04f131820412b9fcfec218a1bc1c2a8d32
gunhee lee <dlrjsgml42@naver.com>
2020-08-06 16:46:32 +0900

    feat: 요금 계산에 대한 간단한 로직 PathResponseAssembler에 추가

--- src/main/java/wooteco/subway/maps/map/dto/PathResponseAssembler.java
+++ src/main/java/wooteco/subway/maps/map/dto/PathResponseAssembler.java
@@ -18,7 +18,18 @@ public class PathResponseAssembler {
 
         //// TODO: 2020/08/06 여기서 요금 관련 로직 넣어주기
         int fare = 1250;
+        fare = calculareFare(distance, fare);
 
-        return new PathResponse(stationResponses, subwayPath.calculateDuration(), distance, 1250);
+        return new PathResponse(stationResponses, subwayPath.calculateDuration(), distance, fare);
+    }
+
+    private static int calculareFare(int distance, int fare) {
+        if(distance >= 10) {
+            fare += (((distance % 50) - 10) / 5) * 100;
+        }
+        if(distance >= 50) {
+            fare += ((distance - 50) / 8)*100;
+        }
+        return fare;
     }
 }
